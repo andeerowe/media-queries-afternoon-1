8aa849ccc83a2a10a3a0f505775fc3a87a662cbf
andeerowe <andeerowe@gmail.com>
2019-10-08 09:34:49 -0600

    Completed Afternoon Project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(){
+    super ()
+
+    this.dropdown = React.createRef()
+  }
+
+toggleShow = () => {
+  let {current} = this.dropdown
+  if(current.classList.contains('show-animation')){
+    current.classList.add('hide-animation')
+    current.classList.remove('show-animation')
+  } else {
+    current.classList.add('show-animation')
+    current.classList.remove('hide-animation')
+  }
+}
+
+  render () {
   return (
     <div className="App">
       <div className="pictureBox">
         <nav>
           <button className="start-bootstrap" ><b>Start Bootstrap</b></button>
-          <div className="menu">MENU<i class="fas fa-bars"></i></div>
+          <div className="menu" onClick={this.toggleShow}>MENU<i class="fas fa-bars"></i></div>
+        
           <div className="button-container">
             <button className="nav-button">SERVICES</button>
             <button className="nav-button">PORTFOLIO</button>
@@ -17,6 +36,13 @@ function App() {
           </div>
         </nav>
         <div className="middleBox">
+        <div className="dropdown"  ref={this.dropdown}>
+          <button className="dropdown-button">SERVICES</button>
+            <button className="dropdown-button">PORTFOLIO</button>
+            <button className="dropdown-button">ABOUT</button>
+            <button className="dropdown-button">TEAM</button>
+            <button className="dropdown-button">CONTACT</button>
+          </div>
           <div className="welcome"><b>Welcome To Our Studio!</b></div>
           <br />
           <div className="nice"> <b>IT'S NICE TO MEET YOU</b></div>
@@ -27,6 +53,7 @@ function App() {
       
     </div>
   );
+}
 }
 
 export default App;
